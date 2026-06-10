c0711bfbe5787f92e2fa96c0d10e3a6d01eef977
FerMoreno <fmorenomarzal@gmail.com>
2026-06-10 20:11:12 +0200

    fix(auth): implementacion para probar servicios de email)

--- backend/auth/src/LightWeight.Auth.Infrastructure/Services/EmailSender.cs
+++ backend/auth/src/LightWeight.Auth.Infrastructure/Services/EmailSender.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Json;
+using System.Net.Mail;
 using LightWeight.Auth.Domain.Services;
 
 namespace LightWeight.Auth.Infrastructure.Services;
@@ -14,11 +15,11 @@ public class EmailSender : IEmailSender
 
     public async Task Send(string recipient, string subject, string body)
     {
-            await _http.PostAsJsonAsync("https://api.resend.com/emails", new {
-            from = "onboarding@resend.dev",
-            to = recipient,
-            subject = subject,
-            html = body
-        });
+        using var client = new SmtpClient("localhost", 1025);
+        var message = new MailMessage("noreply@lightweight.dev", recipient, subject, body)
+        {
+            IsBodyHtml = true
+        };
+        await client.SendMailAsync(message);
     }
 }
