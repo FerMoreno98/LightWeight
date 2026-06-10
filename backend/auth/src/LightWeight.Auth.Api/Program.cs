5c63063307bb50801cf7463a23f1b8c3a373a905
FerMoreno <fmorenomarzal@gmail.com>
2026-06-10 20:18:58 +0200

    feat(auth): AddApplication

--- backend/auth/src/LightWeight.Auth.Api/Program.cs
+++ backend/auth/src/LightWeight.Auth.Api/Program.cs
@@ -1,19 +1,24 @@
 using FluentMigrator.Runner;
 using LightWeight.Auth.Api.Middlewares;
 using LightWeight.Auth.API.Middlewares;
-using LightWeight.Auth.Infrastructure;  
+using LightWeight.Auth.Infrastructure;
+using LightWeight.Auth.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddExceptionHandler<AuthExceptionHandler>();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
+builder.Services.AddControllers(); 
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddInfrastructure();
 
 
 var app = builder.Build();
-
+app.UseAuthentication();                    
+app.UseAuthorization(); 
 
 
 app.UseHttpsRedirection();
@@ -24,6 +29,7 @@ using (var scope = app.Services.CreateScope())
     runner.MigrateUp();
 }
 app.UseExceptionHandler();
+app.MapControllers();
 app.Run();
 
 
