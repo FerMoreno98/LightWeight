5f1202a1fb4f4be396c72ebc6060abafe6d178ce
FerMoreno <fmorenomarzal@gmail.com>
2026-06-11 15:11:46 +0200

    feat(userprofile): Migracion de la tabla User

---
+++ backend/user-profile/src/LightWeight.UserProfile.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,34 @@
+using FluentMigrator.Runner;
+using LightWeight.shared.BuildingBlocks.Persistance;
+using LightWeight.shared.Types;
+using LightWeight.UserProfile.Domain.Repository;
+using LightWeight.UserProfile.Infrastructure.Persistence;
+using LightWeight.UserProfile.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LightWeight.UserProfile.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<UserProfileDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb
+                .AddPostgres()
+                .WithGlobalConnectionString(configuration.GetConnectionString("DefaultConnection"))
+                .ScanIn(typeof(DependencyInjection).Assembly).For.Migrations()
+            )
+            .AddLogging(lb => lb.AddFluentMigratorConsole());
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IUnitOfWork,UnitOfWork>();
+
+            services.AddSingleton<IClock, SystemClock>();
+
+        return services;
+    }
+}
