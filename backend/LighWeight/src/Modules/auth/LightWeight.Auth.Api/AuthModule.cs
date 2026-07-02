12e2eb3cb623fc5014412efd0f8d8caa6209f9e2
Fernando <fmorenomarzal@gmail.com>
2026-07-02 17:46:00 +0200

    refactor(auth): uso de cookies para mandar por http el refreshtoken

--- backend/LighWeight/src/Modules/auth/LightWeight.Auth.Api/AuthModule.cs
+++ backend/LighWeight/src/Modules/auth/LightWeight.Auth.Api/AuthModule.cs
@@ -1,4 +1,5 @@
 
+using System.Security.Claims;
 using LightWeight.Auth.Api.DTOs;
 using LightWeight.Auth.Application.Commands.LoginOtp.SendOtpCode;
 using LightWeight.Auth.Application.Commands.LoginOtp.VerifyOtpCode;
@@ -23,6 +24,18 @@ public static class AuthModule
 
         return app;
     }
+    private static void SetRefreshTokenCookie(HttpContext httpContext, string refreshToken)
+    {
+        // hardcodeando valores solo para probar, cambiarlos a variables de entorno en appsettings
+        httpContext.Response.Cookies.Append("refresh_token", refreshToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,              // solo HTTPS (en dev con http, pon false o usa https local)
+            SameSite = SameSiteMode.Strict,
+            Path = "/api/auth",         // la cookie solo se envía a rutas de auth, no a toda la app
+            Expires = DateTimeOffset.UtcNow.AddDays(30) // según tu política de rotation
+        });
+    }
 
     private static async Task<IResult> SendOtpCode(
         SendOtpCodeRequest request,
@@ -32,7 +45,7 @@ public static class AuthModule
         return TypedResults.Ok();
     }
 
-    private static async Task<Results<Ok<OtpLoginResponse>, UnauthorizedHttpResult>> VerifyOtpCode(
+    private static async Task<Results<Ok<string>, UnauthorizedHttpResult>> VerifyOtpCode(
         VerifyOtpCodeRequest request,
         HttpContext httpContext,
         IMediator mediator,
@@ -48,44 +61,51 @@ public static class AuthModule
             request.Platform, 
             request.Email
         ),ct);
+        SetRefreshTokenCookie(httpContext, result.RefreshToken);
 
-        return result is null
-            ? TypedResults.Unauthorized()
-            : TypedResults.Ok(new OtpLoginResponse(result.RefreshToken,result.AccessToken));
+        return TypedResults.Ok(result.AccessToken);
     }
 
-    private static async Task<Results<Ok<RefreshTokenResponse>, UnauthorizedHttpResult>> RefreshTokenLogin(
-        RefreshTokenRequest request,
+    private static async Task<Results<Ok<string>, UnauthorizedHttpResult>> RefreshTokenLogin(
         IMediator mediator,
         HttpContext httpContext,
         CancellationToken ct)
     {
+        if (!httpContext.Request.Cookies.TryGetValue("refresh_token", out var refreshToken))
+        {
+            return TypedResults.Unauthorized();
+        }
         var ip = httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown";
         var result = await mediator.SendAsync<LoginWithRefreshTokenCommand,LoginRefreshTokenResult>
         (
             new LoginWithRefreshTokenCommand
             (
-                request.RefreshToken,
+                refreshToken,
                 ip
-            ), ct);
+            ), 
+            ct
+        );
 
-        return result is null
-            ? TypedResults.Unauthorized()
-            : TypedResults.Ok(new RefreshTokenResponse(result.AccessToken, result.RefreshToken));
+        SetRefreshTokenCookie(httpContext, result.RefreshToken);
+
+        return TypedResults.Ok(result.AccessToken);
     }
 
     private static async Task<IResult> Logout(
         LogoutRequest request,
         IMediator mediator,
+        HttpContext httpContext,
         CancellationToken ct)
     {
-        await mediator.SendAsync
-        (
-            new LogoutCommand
-            (
-                request.UserId,
-                request.RefreshToken
-            ), ct);
+        var user = httpContext.User; 
+        string? id = user.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                    ?? throw new UnauthorizedAccessException();
+
+        httpContext.Request.Cookies.TryGetValue("refresh_token", out var refreshToken);
+
+        await mediator.SendAsync(new LogoutCommand(Guid.Parse(id), refreshToken), ct);
+
+        httpContext.Response.Cookies.Delete("refresh_token", new CookieOptions { Path = "/api/auth" });
         return TypedResults.NoContent();
     }
 }
